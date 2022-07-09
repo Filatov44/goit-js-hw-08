93c6c99b95875a162ccd07f74dbc4f3e2a71350e
Sergey Filatov <filatov300@gmail.com>
2022-07-09 13:21:59 +0300

    add js files 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,55 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const renderListGalleryEl = renderListGallery(galleryItems);
 
-console.log(galleryItems);
+// Создаем разметку из массива объектов
+
+// function renderListGallery(items) {
+//   return items.reduce(
+//     (acc, { preview, original, description }) =>
+//       acc +
+//       `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`,
+//     ""
+//   );
+// }
+
+function renderListGallery(items) {
+  const result = items
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__list">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+  return result;
+}
+
+// Вставляем разметку на экран
+
+galleryEl.insertAdjacentHTML("beforeend", renderListGalleryEl);
+
+//Активируем библиотеку с задержкой alt
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+//Импортируем Плеер
+import Player from '@vimeo/player';
 
+//Добавляем метод троттл
+const throttle = require('lodash.throttle');
+
+//Импортируем конструктор Плеер
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+//Время воспроизведения (ключь)
+const timePlay = "videoplayer-current-time";
+
+//Метод on
+
+const onPlay = function(data) {
+	localStorage.setItem(timePlay, data.seconds);
+};
+
+player.on("timeupdate", throttle(onPlay, 1000));
+
+//Используем метод setCurrentTime() при перезагрузке страницы
+
+addEventListener("DOMContentLoaded", player.setCurrentTime(localStorage.getItem(timePlay)));
